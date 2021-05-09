16cbec91d1a5af2c9fd34f81c6507424a266425f
satyam <satyamsdet@gmail.com>
2021-05-09 06:06:45 +0000

    Added new class Types casting

---
+++ src/Test/TypeCasting.java
@@ -0,0 +1,22 @@
+package Test;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// implicit Type Casting
+		
+		char a = 'a';
+		System.out.println("value of a :" + a);
+		int b = 'z';
+		System.out.println("value of b :" + b);
+		
+		// explicit type casting
+		
+	   double num = 14.54;
+	   int num1 = (int)num;
+	   System.out.println(num1);
+		
+
+	}
+
+}
