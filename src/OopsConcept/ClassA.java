8f9a3233b1d1e1a5a6495a97f5b42469c33bdbaa
satyam <satyamsdet@gmail.com>
2021-05-09 07:21:46 +0000

    Changes in ClassA

--- src/OopsConcept/ClassA.java
+++ src/OopsConcept/ClassA.java
@@ -8,6 +8,21 @@ public class ClassA {
 		public String Org;
 		public String Deg;
 		
+		public ClassA() {
+			System.out.println("Inside constructor");
+			
+			
+		}
+		public ClassA(int val1, String val2 , String val3 ,String val4) {
+			System.out.println("Inside param constructor");
+			this.Slary = val1;
+			this.Name = val2;
+			this.Org = val3;
+			this.Deg = val4;
+			
+			
+		}
+		
 		public void Display(){
 			System.out.println("Employee Name : " + Name);
 		    System.out.println("Enployee Organisation : " + Org);
